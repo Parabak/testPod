e78e275dc325818a243f4bf7357bbaa1d5821675
Parabak <al.baranovski89@gmail.com>
2017-07-12 11:00:39 +0300

    Initial Commit

---
+++ testPod/testPod.h
@@ -0,0 +1,19 @@
+//
+//  testPod.h
+//  testPod
+//
+//  Created by Aliaksandr Baranouski on 12/07/2017.
+//  Copyright © 2017 Aliaksandr Baranouski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for testPod.
+FOUNDATION_EXPORT double testPodVersionNumber;
+
+//! Project version string for testPod.
+FOUNDATION_EXPORT const unsigned char testPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <testPod/PublicHeader.h>
+
+
